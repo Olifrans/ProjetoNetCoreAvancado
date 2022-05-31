fef5c7b7be120344ed0fe24411ea83bf2bf6fc11
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-05-31 13:14:11 -0300

    feat: Add do FluentValidation

---
+++ NetCore.Domain/Validations/ClientValidation.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCore.Domain.Validations
+{
+    public class ClientValidation : AbstractValidator
+    {
+
+    }
+}
