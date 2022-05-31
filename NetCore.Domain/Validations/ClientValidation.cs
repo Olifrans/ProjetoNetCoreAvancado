7bac8bb5b9d95f1254f4ddb667d368c0606de0f0
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-05-31 13:29:51 -0300

    feat: ClientValidation

--- NetCore.Domain/Validations/ClientValidation.cs
+++ NetCore.Domain/Validations/ClientValidation.cs
@@ -1,14 +1,26 @@
 ﻿using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NetCore.Domain.Models;
 
-namespace NetCore.Domain.Validations
+namespace NetCore.Domain.Validations;
+
+public class ClientValidation : AbstractValidator<ClientModel>
 {
-    public class ClientValidation : AbstractValidator
+    public ClientValidation()
     {
+        RuleFor(x => x.Name)
+            .NotNull()
+            .NotEmpty()
+            .Length(3, 100);
 
+        RuleFor(x => x.Email)
+            .NotNull()
+            .NotEmpty()
+            .EmailAddress(FluentValidation.Validators.EmailValidationMode.AspNetCoreCompatible)
+            .Length(5, 150);
+
+        RuleFor(x => x.PhoneNumber)
+            .NotNull()
+            .NotEmpty()
+            .Length(11, 14);
     }
 }
